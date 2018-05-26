cad9ada5b3358d7708c05a756f4f52a26dece9c1
Anton Ryzhov <Sonic9.anton@gmail.com>
2018-05-26 20:04:55 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+
+});
+
+gulp.task('default',['watch']);
